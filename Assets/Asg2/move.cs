4ededdab435f798255968512a9a181231a970c1a
Wong Yan Zhi <s10247154@connect.np.edu.sg>
2023-06-10 19:57:24 +0800

    Animation1

--- Assets/Asg2/move.cs
+++ Assets/Asg2/move.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro; //TextMeshPro
+using UnityEngine.UI; // Image
+using UnityEngine.SocialPlatforms.Impl;
 
 public class move : MonoBehaviour
 {
     Vector3 movementInput = Vector3.zero;
-    float movementSpeed = 0.09f;
+    float movementSpeed = 0.06f;
 
     Vector3 rotationInput = Vector3.zero;
     float rotationSpeed = 1f;
 
     public Transform Camera;
+    int score = 0;
+    public TextMeshProUGUI displayText;
 
+    public float sprintModifier = 5f;
+    bool sprintKey = false;
+
+    public bool CollectObject(string tag, GameObject obj, int val)
+    {
+        if (obj.tag == tag)
+        {
+            score += val;
+            displayText.text = "Score : " + score;
+            Destroy(obj);
+            return true;
+        }
+        return false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (CollectObject("Collectible", collision.gameObject, 3)) ;
+        else if (CollectObject("Damage", collision.gameObject, -2)) ; 
+    }
     void OnLook(InputValue value)
     {
         rotationInput.y = value.Get<Vector2>().x;
@@ -34,21 +58,26 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 forwardDir = transform.forward;
-        forwardDir *= movementInput.y;
+        //Vector3 forwardDir = transform.forward;
+        //forwardDir *= movementInput.y;
 
-        Vector3 rightDir = transform.right;
-        rightDir *= movementInput.x;
+        //Vector3 rightDir = transform.right;
+        //rightDir *= movementInput.x;
 
-        transform.position += (forwardDir + rightDir) * movementSpeed;
+        //transform.position += (forwardDir + rightDir) * movementSpeed;
 
-        //Rotate left and right
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles
-            + new Vector3(0, rotationInput.y, 0) * rotationSpeed);
+        ////Rotate left and right
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles
+        //    + new Vector3(0, rotationInput.y, 0) * rotationSpeed);
 
-        //Rotate up and down
-        var headRot = Camera.rotation.eulerAngles + new Vector3(rotationInput.x, 0, 0)
-            * rotationSpeed;
-        Camera.rotation = Quaternion.Euler(headRot);
+        ////Rotate up and down
+        //var headRot = Camera.rotation.eulerAngles + new Vector3(rotationInput.x, 0, 0)
+        //    * rotationSpeed;
+        //Camera.rotation = Quaternion.Euler(headRot);
+
+        Vector3 movementVector = transform.forward * movementInput.y;
+        movementVector += transform.right * movementInput.x;
+        transform.position += movementVector * movementSpeed;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + rotationInput * rotationSpeed);
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # I3E_STLD_Asg2
- 
+Weekly Commitment
+
+
+Week05
+First time creating a repository using github github desktop. I thought it was easy thing to do, just a simple reository from the website and connect to github desktop and done...but it wasn't this way. I have to connect to unity also and i totally missed that part.
+
 Week06
-On the week 06 i learned about putting audio into unity. Playing audio when game start and stuff
+On the week 06 i learned about putting audio into unity. Playing audio when game start and stuff. It was very confusing at the starting because i wasn't prepared for a new scene and it was rush.
 
 Week07
 On the Week07 was trying debug the code at the quiz.
 
 Week08
-On the week08, i leanred about
+Finally complete doing repository using github desktop and had to resubmit the link again. Week08 i learned about 
