4c407f8a127c935bbde999c4f959ac34e78cf6f1
Wong Yan Zhi <s10247154@connect.np.edu.sg>
2023-07-02 22:58:38 +0800

    Input Mute

---
+++ Assets/Asg2/muteScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class muteScript : MonoBehaviour
+{
+    private bool isMuted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMuted = false;
+    }
+
+
+
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+    }
+}
